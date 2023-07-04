496ed18f7dd788fcf5288a519213668fea389c03
Mathieu Clément <mcfly1893@gmail.com>
2023-07-04 11:28:17 +0200

    Update info text

--- src/app/events/page.tsx
+++ src/app/events/page.tsx
@@ -9,7 +9,10 @@ export default function Events() {
           paragraphs={[
             'Welcome to Emilie Room Booking App!',
             'On this page, you can see all events across all rooms.',
-            'To book a room for an event, see events per room or update room information, please go to the Rooms page.',
+            'To book a room for an event, click on a date/time slot or select a time range on the calendar.',
+            'To see detailed information about an event and update or delete it, double click on the specific event.',
+            'You can add and modify events at please. However, be civil and responsible and do not mess up with other poeple bookings!',
+            'To see events per room and create, update or delete a room, please go to the Rooms page.',
           ]}
         />
       </div>
--- src/app/rooms/page.tsx
+++ src/app/rooms/page.tsx
@@ -10,6 +10,8 @@ export default function Rooms() {
             'You reached the Rooms page.',
             'Here you can see a list of rooms that you can book using their dedicated calendars.',
             'Each room has an associated color that will help you to distinguish them in the Events page overview.',
+            'To book a room for an event, click on a date/time slot or select a time range on the room dedicated calendar.',
+            'To see detailed information about an event and update or delete it, double click on the specific event.',
             'You can add and modify rooms at please. However, be responsible and do not screw everything up!',
             'The same applies to the booked slots.',
           ]}
